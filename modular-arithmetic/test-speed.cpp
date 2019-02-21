b14b9d9d88a7ab7f97f1079d3c8b087e1c2fd20c
Ivan Lazaric <ivan.lazaric1@gmail.com>
2019-02-21 09:44:03 +0100

    wip

--- modular-arithmetic/test-speed.cpp
+++ modular-arithmetic/test-speed.cpp
@@ -14,20 +14,19 @@ using mint = MintSimple::mint<Mod>;
 
 mint calcMint(){
   mint acc(0);
-  srand(432);
   for (int i = 0; i < Limit; ++i){
     acc += mint::unsafe(2*i+12345);
     acc *= mint::unsafe(5*i+65432);
   } return acc;
 }
 
-inline int add(int a, int b){return (a += b) < Mod ? a : a-Mod;}
-inline int sub(int a, int b){return (a -= b) < 0 ? a+Mod : a;}
-inline int mul(int a, int b){return (int)((long long)a * b % Mod);}
-int calcStupid(){
-  int acc = 0;
-  srand(432);
-  for (int i = 0; i < Limit; ++i){
+using uint = unsigned int;
+inline uint add(uint a, uint b){return (a += b) < Mod ? a : a-Mod;}
+inline uint sub(uint a, uint b){return a < b ? a+(Mod-b) : a-b;}
+inline uint mul(uint a, uint b){return (uint)((unsigned long long)a * b % Mod);}
+uint calcStupid(){
+  uint acc = 0;
+  for (uint i = 0; i < Limit; ++i){
     acc = add(acc, 2*i+12345);
     acc = mul(acc, 5*i+65432);
   } return acc;
@@ -35,9 +34,10 @@ int calcStupid(){
 
 #define DISPLAY_TIMED(expression) {					\
     auto __t1__ = high_resolution_clock::now();				\
-    cout << left << setw(20) << ((string)#expression + ": ");		\
-    cout << left << setw(20) << (expression);				\
+    auto value = (expression);						\
     auto __t2__ = high_resolution_clock::now();				\
+    cout << left << setw(20) << ((string)#expression + ": ");		\
+    cout << left << setw(20) << value;					\
     cout << " [" << right << setfill(' ') << setw(5) <<			\
       duration_cast<milliseconds>(__t2__ - __t1__).count() << "ms]" <<	\
       setfill(' ') << endl;						\
@@ -45,6 +45,9 @@ int calcStupid(){
 
 
 int main(){
+
+  calcMint();
+  calcStupid();
 
   DISPLAY_TIMED(calcMint());
   DISPLAY_TIMED(calcStupid());
